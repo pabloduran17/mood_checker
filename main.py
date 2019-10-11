f10dae44f187241256a319917639dec8bccca9df
Pablo <pablo.duran.online@gmail.com>
2019-10-11 12:00:47 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = input("How are you feeling today mate? ")
+
+if mood == "happy":
+    print("It is great to see you happy")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("cheer up, mate!")
+elif mood == "excited":
+    print("have a drink yourself")
+elif mood == "relaxed":
+    print("give me a break, mate")
+else:
+    print("I don´t recognize this mood")
